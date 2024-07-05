eb0c68567867de5bcd584b10acc6b52e74c97041
Michael Lehenauer <michael@lehenauer.com>
2024-07-05 12:02:46 +0200

    better oil config

--- .config/nvim/lua/custom/plugins/oil.lua
+++ .config/nvim/lua/custom/plugins/oil.lua
@@ -1,3 +1,24 @@
+local git_ignored = setmetatable({}, {
+	__index = function(self, key)
+		local proc = vim.system({ "git", "ls-files", "--ignored", "--exclude-standard", "--others", "--directory" }, {
+			cwd = key,
+			text = true,
+		})
+		local result = proc:wait()
+		local ret = {}
+		if result.code == 0 then
+			for line in vim.gsplit(result.stdout, "\n", { plain = true, trimempty = true }) do
+				-- Remove trailing slash
+				line = line:gsub("/$", "")
+				table.insert(ret, line)
+			end
+		end
+
+		rawset(self, key, ret)
+		return ret
+	end,
+})
+
 return {
 	"stevearc/oil.nvim",
 	-- Optional dependencies
@@ -5,9 +26,25 @@ return {
 	opts = {
 		delete_to_trash = true,
 		view_options = {
-			show_hidden = true,
+			show_hidden = false,
 			is_always_hidden = function(name, _)
-				return name == ".." or name == ".git"
+				return name == ".."
+			end,
+			is_hidden_file = function(name, _)
+				-- dotfiles are always considered hidden
+				-- if vim.startswith(name, ".") then
+				-- 	return true
+				-- end
+				if name == ".git" then
+					return true
+				end
+				local dir = require("oil").get_current_dir()
+				-- if no local directory (e.g. for ssh connections), always show
+				if not dir then
+					return false
+				end
+				-- Check if file is gitignored
+				return vim.list_contains(git_ignored[dir], name)
 			end,
 		},
 	},
