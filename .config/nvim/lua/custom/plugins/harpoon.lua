5b2b7a02241e5c070acbb345cf44cbb331da9c3a
Michael Lehenauer <michael@lehenauer.com>
2024-07-17 19:16:04 +0200

    handle nil values in the list correctly

--- .config/nvim/lua/custom/plugins/harpoon.lua
+++ .config/nvim/lua/custom/plugins/harpoon.lua
@@ -1,6 +1,6 @@
 return {
     "kimabrandt-flx/harpoon",
-    branch = "develop",
+    branch = "feat_sync_selection",
     dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
 
     config = function()
@@ -27,23 +27,52 @@ return {
         harpoon:extend(extensions.builtins.navigate_with_number())
 
         local function cur_buf_is_harpoon()
-            local bufnr = vim.fn.bufnr(
-                vim.loop.fs_realpath(harpoon:list().items[harpoon:list()._index].value)
-            )
+            local current = harpoon:list().items[harpoon:list()._index]
+            if not current then
+                return true
+            end
+
+            local bufnr = vim.fn.bufnr(vim.loop.fs_realpath(current.value))
             return bufnr == vim.fn.bufnr()
+        end
+
+        local function next_harpoon(harpoon_files, prev)
+            local i = harpoon_files._index
+            local j = 1
+            while true do
+                if prev then
+                    i = i - 1
+                else
+                    i = i + 1
+                end
+                if i > #harpoon_files.items then
+                    i = 1
+                end
+                if i <= 0 then
+                    i = #harpoon_files.items
+                end
+                if harpoon_files.items[i] then
+                    harpoon_files:select(i)
+                    return
+                end
+                j = j + 1
+                if j > #harpoon_files.items then
+                    return
+                end
+            end
         end
 
         -- Toggle previous & next buffers stored within Harpoon list
         vim.keymap.set("n", "<S-Tab>", function()
             if cur_buf_is_harpoon() then
-                harpoon:list():prev({ ui_nav_wrap = true })
+                next_harpoon(harpoon:list(), true)
             else
                 harpoon:list():select(harpoon:list()._index)
             end
         end, { desc = "Switch to previous buffer in Harpoon List" })
         vim.keymap.set("n", "<Tab>", function()
             if cur_buf_is_harpoon() then
-                harpoon:list():next({ ui_nav_wrap = true })
+                next_harpoon(harpoon:list(), false)
             else
                 harpoon:list():select(harpoon:list()._index)
             end
@@ -74,7 +103,13 @@ return {
             and (#harpoon:list().items > 0)
             and vim.api.nvim_get_option_value("buftype", { buf = 0 }) == ""
         then
-            harpoon:list():select(1)
+            -- select first harpoon item that isn't nil
+            for i = 1, #harpoon:list().items do
+                if harpoon:list().items[i] then
+                    harpoon:list():select(i)
+                    return
+                end
+            end
             return
         end
     end,
