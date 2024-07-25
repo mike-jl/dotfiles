a8d7bc0edaca969df3ae03fc6b31d3b9e7485200
Michael Lehenauer <michael@lehenauer.com>
2024-07-25 22:20:24 +0200

    fix gopls hints

--- .config/nvim/lua/custom/plugins/lsp.lua
+++ .config/nvim/lua/custom/plugins/lsp.lua
@@ -268,14 +268,16 @@ return { -- LSP Configuration & Plugins
             },
             gopls = {
                 settings = {
-                    hints = {
-                        rangeVariableTypes = true,
-                        parameterNames = true,
-                        constantValues = true,
-                        assignVariableTypes = true,
-                        compositeLiteralFields = true,
-                        compositeLiteralTypes = true,
-                        functionTypeParameters = true,
+                    gopls = {
+                        hints = {
+                            rangeVariableTypes = true,
+                            parameterNames = true,
+                            constantValues = true,
+                            assignVariableTypes = true,
+                            compositeLiteralFields = true,
+                            compositeLiteralTypes = true,
+                            functionTypeParameters = true,
+                        },
                     },
                 },
             },
