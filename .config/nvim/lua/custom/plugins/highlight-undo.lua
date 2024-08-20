fffe3780a6b34c8322e8751aada6020704e114ff
Michael Lehenauer <michael@lehenauer.com>
2024-08-20 12:07:00 +0200

    add highlight undo

---
+++ .config/nvim/lua/custom/plugins/highlight-undo.lua
@@ -0,0 +1,11 @@
+return {
+    "tzachar/highlight-undo.nvim",
+    opts = {
+        undo = {
+            hlgroup = "Search",
+        },
+        redo = {
+            hlgroup = "Search",
+        },
+    },
+}
