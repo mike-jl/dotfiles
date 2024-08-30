1a5f297ddaeefe37766db7d3ff738c7238f9782d
Michael Lehenauer <michael@lehenauer.com>
2024-08-30 11:39:43 +0200

    change lsp rename keymap so its the same as nvim 0.11

--- .config/nvim/lua/custom/plugins/lsp.lua
+++ .config/nvim/lua/custom/plugins/lsp.lua
@@ -96,7 +96,7 @@ return { -- LSP Configuration & Plugins
 
                 -- Rename the variable under your cursor.
                 --  Most Language Servers support renaming across files, etc.
-                map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+                map("grn", vim.lsp.buf.rename, "Rename")
 
                 -- Execute a code action, usually your cursor needs to be on top of an error
                 -- or a suggestion from your LSP for this to activate.
