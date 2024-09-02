3626ffdffd4820a991029e924b94a082f936a9bd
Michael Lehenauer <michael@lehenauer.com>
2024-09-02 08:54:50 +0200

    native to luasnip

--- .config/nvim/lua/custom/plugins/neogen.lua
+++ .config/nvim/lua/custom/plugins/neogen.lua
@@ -1,7 +1,7 @@
 return {
     "danymat/neogen",
     opts = {
-        snippet_engine = "nvim",
+        snippet_engine = "luasnip",
     },
     -- Uncomment next line if you want to follow only stable versions
     -- tag = "*"
